959453721f800d0a0356408a95ced580590c9a50
Kim Jaehong <kjh3420@gmail.com>
2024-06-10 17:40:47 +0900

    change variables and functions

--- js/cursor.js
+++ js/cursor.js
@@ -2,99 +2,79 @@ function handleResize() {
     window.innerWidth !== previousWindowWidth && (clearTimeout(resizeTimer),
         resizeTimer = setTimeout((function () {
             window.location.reload(),
-                previousWindowWidth = window.innerWidth
+                previousWindowWidth = window.innerWidth;
         }
         ), 250))
 }
 
 function prefersReducedMotion() {
-    return window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 }
 
 function initCursorAndButtons(e) {
-    // !1 === generalFlag && (e = document.querySelector("body"));
     e = document.querySelector("body");
-    let t = document.querySelector(".cursor-item");
-    if (!t)
+    let cursor = document.querySelector(".cursor-item");
+    if (!cursor)
         return;
-    let o = 0
-        , r = 0
-        , a = 0
-        , n = 0
-        , i = 0
-        , l = 0
-        , s = 0
-        , c = 0;
-    function d(e, t) {
-        switch (t) {
-            case "top-left":
-                e.style.left = "0px",
-                    e.style.top = "0px",
-                    e.style.transform = "translate(-50%, -50%)";
-                break;
-            case "top-right":
-                e.style.right = "0px",
-                    e.style.top = "0px",
-                    e.style.transform = "translate(50%, -50%)";
-                break;
-            case "bottom-left":
-                e.style.left = "0px",
-                    e.style.bottom = "0px",
-                    e.style.transform = "translate(-50%, 50%)";
-                break;
-            case "bottom-right":
-                e.style.right = "0px",
-                    e.style.bottom = "0px",
-                    e.style.transform = "translate(50%, 50%)"
-        }
-    }
-    !function e() {
-        i += .1 * (o - a),
-            l += .1 * (r - n),
-            i *= .55,
-            l *= .55,
-            a += i,
-            n += l;
-        let d = r - s;
-        var u;
-        c = Math.abs(d) > .2 ? Math.max(Math.min(c + -.1 * d, 90), -90) : (1 - (u = .06)) * c + u * 0,
-            t.style.transform = `translate(${a}px, ${n}px) rotate(${c}deg)`,
-            s = r,
-            requestAnimationFrame(e)
+
+    let currentX = 0,
+        currentY = 0,
+        translateX = 0,
+        translateY = 0,
+        eventX = 0,
+        eventY = 0,
+        ongoingY = 0,
+        currentRotate = 0;
+
+    !function getPosition() {
+        eventX += .1 * (currentX - translateX);
+        eventY += .1 * (currentY - translateY);
+
+        eventX *= .6;
+        eventY *= .6;
+
+        translateX += eventX;
+        translateY += eventY;
+
+        let deg = currentY - ongoingY;
+
+        let hesitater = 0.06;
+
+        currentRotate =
+            Math.abs(deg) > .2
+                ? Math.max(Math.min(currentRotate + -.1 * deg, 90), -90)
+                : (1 - hesitater) * currentRotate + hesitater * 0;
+        cursor.style.transform = `translate(${translateX}px, ${translateY}px) rotate(${currentRotate}deg)`;
+        ongoingY = currentY;
+        requestAnimationFrame(getPosition);
     }(),
-        document.addEventListener("mousemove", (e => {
-            o = e.clientX,
-                r = e.clientY
+        document.addEventListener("mousemove", (cursor => {
+            currentX = cursor.clientX,
+                currentY = cursor.clientY
         }
         )),
-        document.querySelectorAll("[data-cursor]").forEach((e => {
-            e.addEventListener("mouseenter", (function () {
-                const e = document.querySelector(".cursor-item");
-                e && (e.style.display = "flex");
-                const t = this.getAttribute("data-cursor");
-                if (t) {
-                    const e = document.querySelector("[data-cursor-text]");
-                    e && (e.textContent = t)
+        document.querySelectorAll("[data-card]").forEach((card => {
+            const cursor = document.querySelector(".cursor-item");
+
+            card.addEventListener("mouseenter", (function () {
+                cursor && (cursor.style.display = "flex");
+                const dataCard = this.getAttribute("data-card");
+                if (dataCard) {
+                    const dataCursor = document.querySelector("[data-cursor-text]");
+                    dataCursor && (dataCursor.textContent = dataCard)
                 }
             }
             )),
-                e.addEventListener("mouseleave", (function () {
-                    const e = document.querySelector(".cursor-item");
-                    e && (e.style.display = "")
+                card.addEventListener("mouseleave", (function () {
+                    cursor && (cursor.style.display = "")
                 }
                 ))
         }
         ))
 }
+
 function initGeneral(e) {
     initCursorAndButtons(e)
-    // initToolTips(),
-    // initVideoControls(e),
-    // prefersReducedMotion() || (initDocumentClick(),
-    //     setTimeout((() => {
-    //         initHeadlines(e)
-    //     }
-    //     ), 1e3))
 }
 
 initGeneral();
--- js/slider.js
+++ js/slider.js
@@ -23,96 +23,15 @@ function initMobileSliders() {
             }
         }
     })
-    // , t = document.querySelectorAll(".g-nav__item");
-    // function switchSlideThumb(e, t) {
-    //     gsap.to(e, {
-    //         opacity: 0,
-    //         duration: .3,
-    //         ease: "power1.inOut"
-    //     }),
-    //         gsap.to(t, {
-    //             opacity: 1,
-    //             duration: .3,
-    //             ease: "power1.inOut",
-    //             // onComplete: () => {
-    //             //     t.play()
-    //             // }
-    //         })
-    // }
-    // function r(e, t) {
-    //     gsap.to(e, {
-    //         opacity: 1,
-    //         duration: .3,
-    //         ease: "power1.inOut"
-    //     }),
-    //         gsap.to(t, {
-    //             opacity: 0,
-    //             duration: .3,
-    //             ease: "power1.inOut",
-    //             onComplete: () => {
-    //                 t.pause()
-    //             }
-    //         })
-    // }
-    // t[2].classList.add("active"),
-    //     t.forEach(((o, r) => {
-    //         o.addEventListener("click", (function () {
-    //             t.forEach((e => e.classList.remove("active"))),
-    //                 this.classList.add("active"),
-    //                 e.slideTo(r)
-    //         }
-    //         ))
-    //     }
-    // ));
-    // var a = document.querySelector(".story-card.swiper-slide-active");
-
-    // switchSlideThumb(a.querySelector("img"), a.querySelector("video")),
-    //     e.on("beforeSlideChangeStart", (function () {
-    //         var e = document.querySelector(".story-card.swiper-slide-active");
-    //         r(e.querySelector("img"), e.querySelector("video"))
-    //     }
-    //     ))
-    // e.on("slideChangeTransitionEnd", (function () {
-    //     t.forEach((e => e.classList.remove("active")));
-    //     const r = e.activeIndex;
-    //     t[r].classList.add("active");
-    //     var a = document.querySelector(".story-card.swiper-slide-active");
-    //     o(a.querySelector("img"), a.querySelector("video"))
-    // }
-    // ));
-    // const n = new Swiper(".t-card__wrap", {
-    //     slidesPerView: "auto",
-    //     spaceBetween: 0,
-    //     // centeredSlides: !0,
-    //     speed: 800,
-    //     init: !0,
-    //     initialSlide: 2,
-    //     breakpoints: {
-    //         768: {
-    //             init: !1
-    //         }
-    //     }
-    // });
-    // var i = document.querySelector(".t-card.swiper-slide-active");
-    // o(i.querySelector("img"), i.querySelector("video")),
-    // n.on("beforeSlideChangeStart", (function () {
-    //     var e = document.querySelector(".t-card.swiper-slide-active");
-    //     r(e.querySelector("img"), e.querySelector("video"))
-    // }
-    // )),
-    // n.on("slideChangeTransitionEnd", (function () {
-    //     var e = document.querySelector(".t-card.swiper-slide-active");
-    //     o(e.querySelector("img"), e.querySelector("video"))
-    // }
-    // ))
 };
+
 function initCardsHover() {
     document.querySelectorAll("[data-card]").forEach((e => {
-        const t = e.style.zIndex || 0;
+        const zIndex = e.style.zIndex || 0;
         if (!0 === ("static" === e.getAttribute("data-card")))
             return;
-        const o = e.querySelector("video")
-            , r = e.querySelector("img");
+        // const o = e.querySelector("video")
+        const thumb = e.querySelector("img");
         e.addEventListener("mouseenter", (() => {
             e.style.zIndex = 2,
                 gsap.to(e, {
@@ -121,23 +40,25 @@ function initCardsHover() {
                     duration: .6,
                     ease: CustomEase.create("guides-bounce", "M0,0 C0.084,0.61 0.202,0.898 0.327,0.977 0.555,1.121 0.661,0.92 1,1 ")
                 }),
-                supportsTouch() || (gsap.to(r, {
-                    opacity: 0,
+                supportsTouch() || (gsap.to(thumb, {
+                    // opacity: 0,
                     duration: .2,
                     ease: "power2"
-                }),
-                    gsap.to(o, {
-                        opacity: 1,
-                        duration: .2,
-                        ease: "power2",
-                        onComplete: () => {
-                            o.play()
-                        }
-                    }))
+                })
+                    // ,
+                    //     gsap.to(o, {
+                    //         opacity: 1,
+                    //         duration: .2,
+                    //         ease: "power2",
+                    //         onComplete: () => {
+                    //             o.play()
+                    //         }
+                    //     })
+                )
         }
         )),
             e.addEventListener("mouseleave", (() => {
-                e.style.zIndex = t,
+                e.style.zIndex = zIndex,
                     gsap.to(e, {
                         scale: 1,
                         // rotate: prefersReducedMotion() ? 0 : 6 * Math.random() - 3,
@@ -145,67 +66,69 @@ function initCardsHover() {
                         duration: .6,
                         ease: CustomEase.create("guides-bounce", "M0,0 C0.084,0.61 0.202,0.898 0.327,0.977 0.555,1.121 0.661,0.92 1,1 ")
                     }),
-                    supportsTouch() || (gsap.to(r, {
-                        opacity: 1,
+                    supportsTouch() || (gsap.to(thumb, {
+                        // opacity: 1,
                         duration: .2,
                         ease: "power2"
-                    }),
-                        gsap.to(o, {
-                            opacity: 0,
-                            duration: .2,
-                            ease: "power2",
-                            onComplete: () => {
-                                o.pause()
-                            }
-                        }))
+                    })
+                        // ,
+                        //     gsap.to(o, {
+                        //         opacity: 0,
+                        //         duration: .2,
+                        //         ease: "power2",
+                        //         onComplete: () => {
+                        //             o.pause()
+                        //         }
+                        //     })
+                    )
             }
             ))
     }
     ))
 }
 
-function initVideoOnHover() {
-    if (supportsTouch())
-        return;
-    let e = document.querySelectorAll("[data-video-hover]");
-    e && e.forEach((e => {
-        let t = e.querySelector("img")
-            , o = e.querySelector("video");
-        e.addEventListener("mouseenter", (() => {
-            gsap.to(t, {
-                opacity: 0,
-                duration: .2,
-                ease: "power2"
-            }),
-                gsap.to(o, {
-                    opacity: 1,
-                    duration: .2,
-                    ease: "power2",
-                    onComplete: () => {
-                        o.play()
-                    }
-                })
-        }
-        )),
-            e.addEventListener("mouseleave", (() => {
-                gsap.to(t, {
-                    opacity: 1,
-                    duration: .2,
-                    ease: "power2"
-                }),
-                    gsap.to(o, {
-                        opacity: 0,
-                        duration: .2,
-                        ease: "power2",
-                        onComplete: () => {
-                            o.pause()
-                        }
-                    })
-            }
-            ))
-    }
-    ))
-}
+// function initVideoOnHover() {
+//     if (supportsTouch())
+//         return;
+//     let e = document.querySelectorAll("[data-video-hover]");
+//     e && e.forEach((e => {
+//         let t = e.querySelector("img")
+//             , o = e.querySelector("video");
+//         e.addEventListener("mouseenter", (() => {
+//             gsap.to(t, {
+//                 opacity: 0,
+//                 duration: .2,
+//                 ease: "power2"
+//             }),
+//                 gsap.to(o, {
+//                     opacity: 1,
+//                     duration: .2,
+//                     ease: "power2",
+//                     onComplete: () => {
+//                         o.play()
+//                     }
+//                 })
+//         }
+//         )),
+//             e.addEventListener("mouseleave", (() => {
+//                 gsap.to(t, {
+//                     opacity: 1,
+//                     duration: .2,
+//                     ease: "power2"
+//                 }),
+//                     gsap.to(o, {
+//                         opacity: 0,
+//                         duration: .2,
+//                         ease: "power2",
+//                         onComplete: () => {
+//                             o.pause()
+//                         }
+//                     })
+//             }
+//             ))
+//     }
+//     ))
+// }
 initCardsHover();
-initVideoOnHover();
+// initVideoOnHover();
 initMobileSliders();
